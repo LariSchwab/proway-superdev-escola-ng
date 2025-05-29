c405d9deeeb2f55767e9e44032f4ffb6968b6329
LarissaSchwab <larissa.schwab@yahoo.com.br>
2025-05-28 21:21:10 -0300

    ajustado crud aluno e curso

--- src/app/components/cursos/curso-cadastro/curso-cadastro.component.ts
+++ src/app/components/cursos/curso-cadastro/curso-cadastro.component.ts
@@ -6,17 +6,45 @@ import { CursoCadastro } from '../../../curso-cadastro';
 import { InputMaskModule } from 'primeng/inputmask';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
+import { Toast, ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { CursoService } from '../../../services/curso.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-curso-cadastro',
-  imports: [FormsModule, InputTextModule, FloatLabelModule, InputMaskModule, ButtonModule],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    InputMaskModule,
+    ButtonModule,
+    ToastModule,
+  ],
+  providers: [MessageService],
   templateUrl: './curso-cadastro.component.html',
   styleUrl: './curso-cadastro.component.css'
 })
 export class CursoCadastroComponent {
   curso: CursoCadastro;
 
-  constructor(){
+  constructor(
+    private router: Router,
+    private cursoService: CursoService,
+    private messageService: MessageService,
+  ){
     this.curso = new CursoCadastro();
+  }
+
+  cadastrar() {
+    this.cursoService.cadastrar(this.curso).subscribe({
+      next: aluno => this.apresentarMensagemCadastrado(),
+      error: erro => console.log("Ocorreu um erro ao cadastrar o aluno:" + erro),      
+    })
+  }
+
+  private apresentarMensagemCadastrado() {
+    this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Curso cadastrado com sucesso'});
+    this.router.navigate(["/cursos"]);
   }
 }
